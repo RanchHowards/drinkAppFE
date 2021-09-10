6cf264582fd8dd5b7e450ea25e520cad424a753e
RanchHowards <69303624+RanchHowards@users.noreply.github.com>
2021-09-10 19:58:06 +0200

    add login & create user capabilities. start (but not yet complete) CONTEXT argument of graphql to access current user info

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './App.css'
 import Events from './components/Events'
 
 import { useMutation } from '@apollo/client'
-import { ADD_EVENT, ALL_EVENTS } from './queries'
+import { ADD_EVENT, ALL_EVENTS, CREATE_USER, LOGIN } from './queries'
 
 function App() {
   //sign in
@@ -13,31 +13,71 @@ function App() {
   //edit profile
   //show posts
 
-  const [signedIn, setSignIn] = useState(false)
+  //STATE
   const [eventName, setEventName] = useState('')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [showSignIn, setShowSignIn] = useState(false)
+  const [showSignUp, setShowSignUp] = useState(false)
+  const [token, setToken] = useState(null)
+
+  //MUTATIONS
+  const [createUser] = useMutation(CREATE_USER, {
+    onError: (err) =>
+      console.log('error from createUser mutation in App.js', err),
+  })
+  const [login, result] = useMutation(LOGIN, {
+    onError: (err) => console.log('error from LOGIN mutation in App.js', err),
+  })
+
+  const [addEvent] = useMutation(ADD_EVENT, {
+    refetchQueries: [{ query: ALL_EVENTS }],
+    onError: (error) => console.log(error),
+  })
+
+  //checks for token of previous session
+  useEffect(() => {
+    const previousToken = localStorage.getItem('user-token')
+    if (previousToken) {
+      setToken(previousToken)
+    }
+  }, [])
+
+  //sets token after loggin in
+  useEffect(() => {
+    if (result.data) {
+      setToken(result.data.login.value)
+      localStorage.setItem('user-token', token)
+    }
+  }, [result.data]) // eslint-disable-line
 
   const signIn = () => {
     if (!showSignIn) {
       setShowSignIn(true)
     } else {
-      setSignIn(true)
+      login({ variables: { username, password } })
+
       setShowSignIn(false)
+      setUsername('')
+      setPassword('')
+    }
+  }
+  const signUp = () => {
+    if (!showSignUp) {
+      setShowSignUp(true)
+    } else {
+      createUser({ variables: { username, password } })
+      setShowSignUp(false)
       setUsername('')
       setPassword('')
     }
   }
 
   const signOut = () => {
-    setSignIn(false)
+    //must still clear cache & ....
+    localStorage.clear()
+    setToken(null)
   }
-
-  const [addEvent] = useMutation(ADD_EVENT, {
-    refetchQueries: [{ query: ALL_EVENTS }],
-    onError: (error) => console.log(error),
-  })
 
   const handleEvent = (event) => {
     event.preventDefault()
@@ -46,13 +86,34 @@ function App() {
     setEventName('')
   }
 
-  if (!signedIn)
+  if (!token)
     return (
       <div>
         <div>
           <button onClick={signIn}>sign in</button>
-          <button>sign up</button>
+          <button onClick={signUp}>sign up</button>
           {showSignIn && (
+            <div>
+              <form>
+                <div>
+                  username
+                  <input
+                    value={username}
+                    onChange={({ target }) => setUsername(target.value)}
+                  ></input>
+                </div>
+                <div>
+                  password
+                  <input
+                    value={password}
+                    onChange={({ target }) => setPassword(target.value)}
+                    type="password"
+                  ></input>
+                </div>
+              </form>
+            </div>
+          )}
+          {showSignUp && (
             <div>
               <form>
                 <div>
--- src/queries.js
+++ src/queries.js
@@ -28,3 +28,19 @@ export const ADD_EVENT = gql`
     }
   }
 `
+
+export const CREATE_USER = gql`
+  mutation createUser($username: String!, $password: String!) {
+    createUser(username: $username, password: $password) {
+      username
+    }
+  }
+`
+
+export const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      value
+    }
+  }
+`
